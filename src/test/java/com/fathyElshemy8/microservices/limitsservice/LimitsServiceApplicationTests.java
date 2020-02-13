bf39a2e5f9a81139366df50d0131be82f1d1229c
fathyelshemy <fathy.elshemy.ext@orange.com>
2020-02-13 08:44:57 +0200

    add main structure of limit-service

---
+++ src/test/java/com/fathyElshemy8/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fathyElshemy8.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
